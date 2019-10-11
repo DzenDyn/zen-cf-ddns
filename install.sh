4d27f9ccf4eae500a2ef5c5ad92f14cc6d4b23a2
DzenDyn <dzendyn@gmail.com>
2019-10-11 19:15:23 +0300

    Change to using venv

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo -e "\e[91mRun installation with sudo to continue"
 echo -e "\e[0m"
 exit
 fi
-echo -e "Creating user"
-sudo useradd -r -s /bin/false zen-cf-ddns
-sudo touch /var/log/zen-cf-ddns.log
-sudo chown zen-cf-ddns /var/log/zen-cf-ddns.log
+sudo python3 -m venv /usr/lib/zen-cf-ddns
+source /usr/lib/zen-cf-ddns/bin/activate
+python -m pip install -upgrade pip
+python -m pip install -r requirements.txt
 sudo cp zen-cf-ddns.service /lib/systemd/system/zen-cf-ddns.service
 sudo cp zen-cf-ddns.py /usr/bin/zen-cf-ddns.py
 sudo cp zen-cf-ddns.conf /etc/zen-cf-ddns.conf
