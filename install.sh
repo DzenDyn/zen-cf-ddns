e41cb93b4599aa4e8f0570684c419ef3fcb02cb3
DzenDyn <dzendyn@gmail.com>
2021-01-15 14:45:47 +0000

    install.sh fixed & tested for Ubuntu 20.04

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+\#!/bin/bash
 isroot=$(id -u)
 if [[ "$isroot" != 0 ]]; then
 echo -e "\e[91mRun installation with sudo to continue"
@@ -8,9 +8,9 @@ fi
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3-venv
-sudo python3 -m venv /usr/lib/zen-cf-ddns
-/usr/lib/zen-cf-ddns/bin/pip install --upgrade pip
-/usr/lib/zen-cf-ddns/bin/pip install -r requirements.txt
+sudo python3.8 -m venv /usr/lib/zen-cf-ddns
+/usr/lib/zen-cf-ddns/bin/pip3 install --upgrade pip
+/usr/lib/zen-cf-ddns/bin/pip3 install -r requirements.txt
 sudo cp zen-cf-ddns.service /lib/systemd/system/zen-cf-ddns.service
 sudo cp zen-cf-ddns.py /usr/bin/zen-cf-ddns.py
 sudo cp zen-cf-ddns.conf /etc/zen-cf-ddns.conf
